4542a53fbb42444afd8bdd0fcf9d260ba2891c0d
BADRYZ <badryzakaria1@gmail.com>
2024-05-11 21:11:46 +0100

    v zak

--- bib/app/helpers/sessions_helper.rb
+++ bib/app/helpers/sessions_helper.rb
@@ -2,4 +2,8 @@ module SessionsHelper
   def admin_signed_in?
     current_admin.present?
   end
+  def log_out
+    session.delete(:utilisateur_id)
+    @utilisateur_courant=nil
+  end
 end
--- bib/config/routes.rb
+++ bib/config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   get 'login', to: 'sessions#new', as: 'login'
   post 'login', to: 'sessions#create'
-  delete 'logout', to: 'sessions#destroy', as: 'logout'
+  #delete 'logout', to: 'sessions#destroy', as: 'logout'
+  delete '/logout', to: 'sessions#destroy'
+
 
 
 
